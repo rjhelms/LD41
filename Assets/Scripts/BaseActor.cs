428976150e252d7e7ce0fd46548fc1059265c509
Rob Hailman <robhailman@gmail.com>
2018-04-21 16:17:02 -0400

    Create BaseActor
    lots of stuff that any ol' character is going to need, so move it up to
    BaseActor

---
+++ Assets/Scripts/BaseActor.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseActor : MonoBehaviour
+{
+    public Sprite IdleSprite;
+    public Sprite[] WalkSprites;
+    public Sprite[] AttackSprites;
+    public Sprite JumpSprite;
+    public float WalkSpriteTime;
+    public float PunchSpriteTime = 1.0f;
+
+    public int Facing = 1;
+    public float WalkSpeed = 1.0f;
+    public int AnimState = 0;
+    public int AnimSpriteCount = 0;
+    public float AnimNextSpriteTime = 0;
+    public float AttackEndTime = 0;
+
+    public int LeftBound = 24;
+    public int RightBound = 296;
+    public int UpperBound = 96;
+    public int LowerBound = 8;
+
+    public bool IsJumping = false;
+    public float JumpStartSpeed = 10.0f;
+    public float JumpCurrentSpeed = 10.0f;
+    public bool JumpAccelFrame = false;
+
+    protected SpriteRenderer spriteRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        transform.localScale = new Vector3(Facing, 1, 0);
+
+        switch (AnimState)
+        {
+            case 0: // idle
+                spriteRenderer.sprite = IdleSprite;
+                break;
+            case 1: // walk
+                if (Time.time > AnimNextSpriteTime)
+                {
+                    AnimSpriteCount++;
+                    if (AnimSpriteCount >= WalkSprites.Length)
+                        AnimSpriteCount = 0;
+                    AnimNextSpriteTime += WalkSpriteTime;
+                }
+                spriteRenderer.sprite = WalkSprites[AnimSpriteCount];
+                break;
+            case 2: // punch
+                if (Facing == 1)
+                {
+                    spriteRenderer.sprite = AttackSprites[1];
+                } else
+                {
+                    spriteRenderer.sprite = AttackSprites[0];
+                }
+                break;
+            case 3: // jump
+                spriteRenderer.sprite = JumpSprite;
+                break;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,49 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : BaseActor {
 
-    public Sprite IdleSprite;
-    public Sprite[] WalkSprites;
-    public Sprite[] PunchSprites;
-    public Sprite JumpSprite;
-    public float WalkSpriteTime;
-    public float PunchSpriteTime = 1.0f;
-
-    public int Facing = 1;
-    public float WalkSpeed = 1.0f;
-    public int AnimState = 0;
-    public int AnimSpriteCount = 0;
-    public float AnimNextSpriteTime = 0;
-    public float PunchEndTime = 0;
-
-    public int LeftBound = 24;
-    public int RightBound = 296;
-    public int UpperBound = 96;
-    public int LowerBound = 8;
-
-    public bool IsJumping = false;
-    public float JumpStartSpeed = 10.0f;
-    public float JumpCurrentSpeed = 10.0f;
-    public bool JumpAccelFrame = false;
-    private SpriteRenderer spriteRenderer;
-
-	// Use this for initialization
-	void Start () {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.localScale = new Vector3(Facing, 1, 0);
-        
+    // Update is called once per frame
+    protected override void Update () {
 
         // punch
         if (Input.GetButtonDown("Fire1"))
         {
             if (AnimState < 2)
             {
-                DoPunch();
+                DoAttack();
             }
         }
 
@@ -56,36 +24,8 @@ public class PlayerController : MonoBehaviour {
                 DoJump();
             }
         }
-
-        switch (AnimState)
-        {
-            case 0: // idle
-                spriteRenderer.sprite = IdleSprite;
-                break;
-            case 1: // walk
-                if (Time.time > AnimNextSpriteTime)
-                {
-                    AnimSpriteCount++;
-                    if (AnimSpriteCount >= WalkSprites.Length)
-                        AnimSpriteCount = 0;
-                    AnimNextSpriteTime += WalkSpriteTime;
-                }
-                spriteRenderer.sprite = WalkSprites[AnimSpriteCount];
-                break;
-            case 2: // punch
-                if (Facing == 1)
-                {
-                    spriteRenderer.sprite = PunchSprites[1];
-                } else
-                {
-                    spriteRenderer.sprite = PunchSprites[0];
-                }
-                break;
-            case 3: // jump
-                spriteRenderer.sprite = JumpSprite;
-                break;
-        }
-	}
+        base.Update();
+    }
 
     private void FixedUpdate()
     {
@@ -158,7 +98,7 @@ public class PlayerController : MonoBehaviour {
         }
 
         // don't end punch state early for walk or idle
-        if (AnimState == 2 & newState < 2 & Time.time < PunchEndTime)
+        if (AnimState == 2 & newState < 2 & Time.time < AttackEndTime)
         {
             return;
         }
@@ -186,7 +126,7 @@ public class PlayerController : MonoBehaviour {
         {
             AnimSpriteCount = 0;
             AnimNextSpriteTime = 0;
-            PunchEndTime = Time.time + PunchSpriteTime;
+            AttackEndTime = Time.time + PunchSpriteTime;
             AnimState = 2;
             return;
         }
@@ -206,7 +146,7 @@ public class PlayerController : MonoBehaviour {
 
     }
     
-    private void DoPunch()
+    private void DoAttack()
     {
         // logic for punching goes here
         ChangeAnimState(2);
