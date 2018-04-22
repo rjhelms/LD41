24457815288c9e4e8a9a20ee11c5fcf7c0c5d8be
Rob Hailman <robhailman@gmail.com>
2018-04-22 14:55:29 -0400

    Add sailor track randomness

--- Assets/Scripts/Actors/Sailor.cs
+++ Assets/Scripts/Actors/Sailor.cs
@@ -7,6 +7,9 @@ public class Sailor : BaseEnemy
     [Header("Player tracking")]
     public float TrackChance = 0.5f;
     public Transform PlayerTransform;
+    public Vector2 TrackRandomVector;
+    public int TrackRandomness;
+    public float TrackRandomTime;
 
     [Header("Attack properites")]
     public float AttackDistance;
@@ -14,12 +17,22 @@ public class Sailor : BaseEnemy
     public Collider2D PunchCollider;
     #endregion
 
+    private float nextTrackRandomTime;
     protected override void Start()
     {
         base.Start();
         PlayerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        GenerateTrackRandomness();
     }
 
+    protected override void Update()
+    {
+        base.Update();
+        if (Time.time > nextTrackRandomTime)
+        {
+            GenerateTrackRandomness();
+        }
+    }
     private void FixedUpdate()
     {
         if (Active & gameController.State == GameState.RUNNING)
@@ -65,20 +78,36 @@ public class Sailor : BaseEnemy
 
     private Vector3 TrackPlayer()
     {
+        Vector2 targetPosition = new Vector2(PlayerTransform.position.x, PlayerTransform.position.z);
+        targetPosition += TrackRandomVector;
+        if (targetPosition.x < LeftBound)
+        {
+            targetPosition += new Vector2(LeftBound - targetPosition.x, 0);
+        } else if (targetPosition.x > RightBound)
+        {
+            targetPosition -= new Vector2(RightBound - targetPosition.x, 0);
+        }
+        if (targetPosition.y > UpperBound)
+        {
+            targetPosition -= new Vector2(0, UpperBound - targetPosition.y);
+        } else if (targetPosition.y < LowerBound)
+        {
+            targetPosition += new Vector2(0, LowerBound - targetPosition.y);
+        }
         int xTrack = 0;
         int yTrack = 0;
-        if (PlayerTransform.position.x < transform.position.x)
+        if (targetPosition.x < transform.position.x)
             xTrack = -1;
-        if (PlayerTransform.position.x > transform.position.x)
+        if (targetPosition.x > transform.position.x)
             xTrack = 1;
 
         if (Random.value >= TrackChance)
             xTrack = 0;
 
         // track player Z, to follow the base of the jump
-        if (PlayerTransform.position.z < transform.position.y)
+        if (targetPosition.y < transform.position.y)
             yTrack = -1;
-        if (PlayerTransform.position.z > transform.position.y)
+        if (targetPosition.y > transform.position.y)
             yTrack = 1;
 
         if (Random.value >= TrackChance)
@@ -92,5 +121,12 @@ public class Sailor : BaseEnemy
         // logic for punching goes here
         PunchCollider.enabled = true;
         ChangeAnimState(AnimState.ATTACK);
+    }
+
+    private void GenerateTrackRandomness()
+    {
+        Vector2 randomVector = Random.insideUnitCircle * TrackRandomness;
+        TrackRandomVector = new Vector2(Mathf.RoundToInt(randomVector.x), Mathf.RoundToInt(randomVector.y));
+        nextTrackRandomTime = Time.time + TrackRandomTime;
     }
 }
