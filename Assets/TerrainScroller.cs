94fc71b84671faa8f764e40b0d2307f8271830dd
Rob Hailman <robhailman@gmail.com>
2018-04-22 09:46:25 -0400

    Add scrolling terrain support
    See level1 sprite & prefab for reference implementation

---
+++ Assets/TerrainScroller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainScroller : MonoBehaviour {
+
+    public int ScrollAmount = 360;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "MainCamera")
+        {
+            if (transform.position.x < collision.transform.position.x)
+            {
+                Debug.Log("Off the edge!");
+                transform.position += new Vector3(ScrollAmount, 0, 0);
+            }
+        }
+    }
+}
