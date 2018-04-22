d0d5b997799d36dc3c0b753fe46df459d93c4c67
Rob Hailman <robhailman@gmail.com>
2018-04-21 23:51:03 -0400

    Remove empty methods in Projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,17 +4,6 @@ using System.Collections;
 public class Projectile : MonoBehaviour
 {
     public float Velocity;
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     private void FixedUpdate()
     {
